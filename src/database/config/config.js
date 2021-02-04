b525a207523708a684fed50563accb00ad0d3991
omodauda <omodauda.dl@gmail.com>
2021-02-04 12:55:03 +0100

    chore: refactor db connection

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -2,27 +2,17 @@ require('dotenv').config();
 
 module.exports = {
   development: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DEV_DB,
-    host: "localhost",
+    url: process.env.DEV_DB,
     dialect: "postgres",
     logging: false
   },
   test: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.TEST_DB,
-    host: "localhost",
+    url: process.env.TEST_DB,
     dialect: "postgres",
     logging: false
   },
   production: {
     url: process.env.DATABASE_URL,
-    username: process.env.DATABASE_USER,
-    password: process.env.DATABASE_PASSWORD,
-    database: process.env.DATABASE_DB,
-    host: process.env.DATABASE_HOST,
     dialect: "postgres",
     dialectOptions: {
       ssl: {
--- src/database/models/index.js
+++ src/database/models/index.js
@@ -9,10 +9,19 @@ const config = require(`${__dirname}/../config/config.js`)[env];
 const db = {};
 
 let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+if (config.url) {
+  sequelize = new Sequelize(config.url, config);
 } else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
+  sequelize = new Sequelize(
+    config.database, 
+    config.username, 
+    config.password,
+    {
+      host: config.host,
+      dialect: "postgres"
+    },
+    config
+  );
 }
 
 fs
