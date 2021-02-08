b73242e61fd88122e4b270bfba517905f4e2f3df
omodauda <omodauda.dl@gmail.com>
2021-02-08 14:37:34 +0100

    chore: fix db init

--- src/config/db.js
+++ src/config/db.js
@@ -1,6 +1,10 @@
 import { sequelize } from '../database/models';
 
 export default async function init() {
+  const allowedEnv = ['development', 'production'];
+  if (!process.env.NODE_ENV.includes(allowedEnv)) {
+    return;
+  }
   try {
     await sequelize.authenticate();
     console.log('Database connection established');
