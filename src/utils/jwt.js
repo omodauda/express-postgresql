ad2ad4d1f443a786e1b28989bccc789e367b16cc
omodauda <omodauda.dl@gmail.com>
2021-02-09 15:11:21 +0100

    test auth middleware

--- src/utils/jwt.js
+++ src/utils/jwt.js
@@ -3,6 +3,15 @@ import { User } from '../database/models';
 
 // eslint-disable-next-line consistent-return
 export default async (req, res, next) => {
+  if (!req.headers.authorization) {
+    return res
+      .status(401)
+      .json({
+        status: 'fail',
+        error: 'Unauthorized request',
+      });
+  }
+
   try {
     const token = req.headers.authorization.split(' ')[1];
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
